800cd8c53c2367fad036aa2deffbd4c1db532a8d
Noitidart <noitidart@gmail.com>
2014-05-14 05:31:15 -0700

    initial.rev1
    -when select tab if its loading using binded trick but handleEvent
    -learned:when addeventlistener and have to use .bind(this) should do it
    to the function like var blah = (function...).bind(this) then just
    addeventlistener(..,blah) rather than
    addeventlistener(..,blah.bind(this)) because cannot removeeventlistener
    with just removeeventlistener(..,blah) and cannot do
    removeeventlistener(..,blah.bind(this))

--- bootstrap.js
+++ bootstrap.js
@@ -10,19 +10,12 @@ const myServices = {};
 var cssUri;
 
 Cu.import('resource://gre/modules/Services.jsm');
-Cu.import("resource:///modules/CustomizableUI.jsm");
+Cu.import('resource:///modules/CustomizableUI.jsm');
 Cu.import('resource://gre/modules/devtools/Console.jsm');
 Cu.import('resource://gre/modules/XPCOMUtils.jsm');
 XPCOMUtils.defineLazyGetter(myServices, 'sss', function(){ return Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService) });
 
-function tabSelected(e) {
-	var tab = e.target;
-	var browser = tab.linkedBrowser;
-	var throbber = tab.ownerDocument.getAnonymousElementByAttribute(tab, "class", "tab-throbber"); 
-	console.log('throbber = ', throbber);
-	console.log('e on tabselect = ', e);
-}
-
+var workers = [];
 
 var mutConfig = {attributes: true};
 function mutFunc(ms) {
@@ -38,14 +31,58 @@ function mutFunc(ms) {
 	}
 }
 
+function winWorker(aDOMWindow) {
+	this.DOMWindow = aDOMWindow;
+	this.DOMDocument = this.DOMWindow.document;
+	this.gBrowser = this.DOMWindow.gBrowser;
+	this.gThrobber = this.DOMDocument.getElementById('navigator-throbber');
+	
+	if (!this.gBrowser) {
+		console.error('this window does not have gBrowser');
+	}
+	if (!this.gThrobber) {
+		console.error('this window does not have gThrobber');
+	}
+	if (!this.gBrowser.tabContainer) {
+		console.warn('this window does not have tabContainer, but just an exception/warning NOT an error');
+	}
+	
+	this.handleEvent = function(e) {
+		switch(e.type) {
+			case 'TabSelect':
+				console.log('tab seld yaaa ' + new Date().getTime());
+				var tab = e.target;
+				//var browser = tab.linkedBrowser;
+				var throbber = tab.ownerDocument.getAnonymousElementByAttribute(tab, 'class', 'tab-throbber')
+				if (throbber.hasAttribute('busy')) {
+					this.gThrobber.setAttribute('loading', 'true');
+				} else {
+					this.gThrobber.removeAttribute('loading');
+				}
+				
+				break;
+			default:
+				console.error('e.type not recognized in hanldeEvent', 'e.type = ', e.type);
+		}
+	}
+	
+	this.init = function() {
+		this.gBrowser.tabContainer.addEventListener('TabSelect', this, false);
+	}
+	
+	this.destroy = function() {
+		this.gBrowser.tabContainer.removeEventListener('TabSelect', this, false);
+	}
+}
+
 /*start - windowlistener*/
 var windowListener = {
 	//DO NOT EDIT HERE
 	onOpenWindow: function (aXULWindow) {
 		// Wait for the window to finish loading
 		let aDOMWindow = aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-		aDOMWindow.addEventListener("load", function () {
-			aDOMWindow.removeEventListener("load", arguments.callee, false);
+		aDOMWindow.addEventListener('load', function () {
+			aDOMWindow.removeEventListener('load', arguments.callee, false);
 			windowListener.loadIntoWindow(aDOMWindow, aXULWindow);
 		}, false);
 	},
@@ -72,6 +109,16 @@ var windowListener = {
 		}
 		//Stop listening so future added windows dont get this attached
 		Services.wm.removeListener(windowListener);
+		
+		[].forEach.call(workers, function(worker, i) {
+			//need to check if worker.DOMWindow is open BUT for now im just doing try catch
+			try {
+				worker.destroy();
+			} catch(ex) {
+				console.warn('exception while destroying worker i = ',  i, 'ex = ', ex);
+			}
+			console.log('done destorying worker i = ',  i);
+		});
 	},
 	//END - DO NOT EDIT HERE
 	loadIntoWindow: function (aDOMWindow, aXULWindow) {
@@ -85,7 +132,10 @@ var windowListener = {
 		} */
 		
 		if (aDOMWindow.gBrowser && aDOMWindow.gBrowser.tabContainer) {
-			aDOMWindow.gBrowser.tabContainer.addEventListener("TabSelect", tabSelected, false);
+			//aDOMWindow.gBrowser.tabContainer.addEventListener("TabSelect", tabSelected, false);
+			var worker = new winWorker(aDOMWindow);
+			worker.init();
+			workers.push(worker);
 		}
 		
 	},
@@ -94,9 +144,9 @@ var windowListener = {
 			return;
 		}
 		
-		if (aDOMWindow.gBrowser && aDOMWindow.gBrowser.tabContainer) {
-			aDOMWindow.gBrowser.tabContainer.removeEventListener("TabSelect", tabSelected, false);
-		}
+		// if (aDOMWindow.gBrowser && aDOMWindow.gBrowser.tabContainer) {
+			// aDOMWindow.gBrowser.tabContainer.removeEventListener("TabSelect", tabSelected, false);
+		// }
 	}
 };
 /*end - windowlistener*/
@@ -104,10 +154,10 @@ function startup(aData, aReason) {
 	console.log('0');
 	self.aData = aData; //must go first, because functions in loadIntoWindow use self.aData
 	CustomizableUI.createWidget(
-		  { id : "navigator-throbber",
+		  { id : 'navigator-throbber',
 			defaultArea : CustomizableUI.AREA_NAVBAR,
 			tooltiptext: 'navigator-throbber',
-			label : "navigator-throbber"
+			label : 'navigator-throbber'
 		  });
 	console.log('1');
 	var newURIParam = {
@@ -128,7 +178,7 @@ function startup(aData, aReason) {
 function shutdown(aData, aReason) {
 	if (aReason == APP_SHUTDOWN) return;
 	windowListener.unregister();
-	CustomizableUI.destroyWidget("navigator-throbber");
+	CustomizableUI.destroyWidget('navigator-throbber');
 	myServices.sss.unregisterSheet(cssUri, myServices.sss.USER_SHEET); //running htis last as i think its syncronus
 }
 
